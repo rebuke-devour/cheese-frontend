e024ab51df6ba2e4d0ad1ecf569e05507345d95d
John Torres <rebuke.devour@gmail.com>
2021-11-15 02:10:46 -0500

    cheese frontend

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import Header from "./components/Header.js"
 import Main from "./components/Main.js"
-import Index from "./pages/Index.js"
-import Show from "./pages/Show.js"
+
 
 import './App.scss';
 
